7397b854a25175aa569194ba77e1d36c116df711
Sirius-18 <97686497+Vignesh-Sivaraman@users.noreply.github.com>
2022-08-19 17:43:55 +0530

    Task completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./css/sb-admin-2.css";
+import DashBoard from "./DashBoard";
+
+import Users from "./Users";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Products from "./Products";
+import CreateUser from "./CreateUser";
+import CreateProduct from "./CreateProduct";
+import Portal from "./Portal";
+import Login from "./Login";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/portal" element={<Portal />}>
+          <Route path="dashboard" element={<DashBoard />}></Route>
+          <Route path="users" element={<Users />}></Route>
+          <Route path="create-user" element={<CreateUser />}></Route>
+          <Route path="products" element={<Products />}></Route>
+          <Route path="create-product" element={<CreateProduct />}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
